e6861ad3b049cb6f0865a88225425c1ef30920ff
NillJr <nill-junior@outlook.com>
2021-09-29 09:19:11 -0300

    Initial_commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
-import logo from "./logo.svg";
 import "./App.css";
-import { useEffect, useState } from "react";
-import CharCard from "./components/CharCard";
 import Characters from "./components/Characters";
 
 function App() {
